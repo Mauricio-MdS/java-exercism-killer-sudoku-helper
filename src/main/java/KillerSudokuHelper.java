47c05d6343dd602eab81f76e3c9456a82e2251e8
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-05-08 19:47:21 -0300

    Exercise solution

--- src/main/java/KillerSudokuHelper.java
+++ src/main/java/KillerSudokuHelper.java
@@ -4,11 +4,31 @@ import java.util.List;
 public class KillerSudokuHelper {
 
     List<List<Integer>> combinationsInCage(Integer cageSum, Integer cageSize, List<Integer> exclude) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<List<Integer>> combinations = new ArrayList<>();
+        if (cageSize == 1) {
+            if (!exclude.contains(cageSum)) combinations.add(new ArrayList<>(List.of(cageSum)));
+            return combinations;
+        }
+
+        for (int i = 1; i < 9 && i < cageSum; i++) {
+            if (exclude.contains(i)) continue;
+            List<Integer> newExcluded = new ArrayList<>(exclude);
+            newExcluded.add(i);
+            final int firstElement = i;
+            var otherElements = combinationsInCage(cageSum - i, cageSize - 1, newExcluded);
+            otherElements.forEach(l -> {
+                if (!l.isEmpty()) {
+                    l.add(firstElement);
+                    l.sort(Integer::compareTo);
+                    if (!combinations.contains(l)) combinations.add(l);
+                }
+            });
+        }
+        return combinations;
     }
 
     List<List<Integer>> combinationsInCage(Integer cageSum, Integer cageSize) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return combinationsInCage(cageSum, cageSize, new ArrayList<>());
     }
 
 }
